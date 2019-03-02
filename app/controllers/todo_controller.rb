8172f55b74e4b34c2f8bdd95add868b4afebd65c
Tenzin Shakya <tenzinshakya21@gmail.com>
2019-03-02 19:35:50 +0000

    Variables

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,16 @@ class TodoController < ApplicationController
     end
     
     def show
-        @todo_description= "Make Dinner"
-        @todo_pomodoro_estimate=5
+        todo_id= params[:id]
+        if todo_id=='1'
+            @todo_description="Go Fix MacBook Pro At the Apple Store"
+            @todo_pomodoro_estimate=8
+        elsif todo_id=='2'
+            @todo_description="Go to the Grocery Store"
+            @todo_pomodoro_estimate=5
+        elsif todo_id=='3'
+            @todo_description="Go to the Soup Kitchen to Volunteer"
+            @todo_pomodoro_estimate=6
+        end
     end
 end
