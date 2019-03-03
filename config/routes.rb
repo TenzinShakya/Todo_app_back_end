f8dbf5655e3dad057217cf7fd3d2ae1073ad9a64
Tenzin Shakya <tenzinshakya21@gmail.com>
2019-03-03 19:43:02 +0000

    Id

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-    get 'todo/index', to:'todo#index'
+    root to:'todo#index'
     get 'todo/show/:id', to: 'todo#show'
     get 'todo/new' , to: 'todo#new'
     post 'todo/new', to: 'todo#create'
+    get 'todo/edit/:id', to: 'todo#edit'
+    post 'todo/edit/:id', to: 'todo#update'
+    get 'todo/destroy/:id', to: 'todo#destroy'
 end
