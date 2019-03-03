1a0e62bb80f55ef85d401192490bb70dcab164f1
Tenzin Shakya <tenzinshakya21@gmail.com>
2019-03-03 20:17:25 +0000

    Revert "Id"
    This reverts commit f8dbf5655e3dad057217cf7fd3d2ae1073ad9a64.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-    root to:'todo#index'
+    get 'todo/index', to:'todo#index'
     get 'todo/show/:id', to: 'todo#show'
     get 'todo/new' , to: 'todo#new'
     post 'todo/new', to: 'todo#create'
-    get 'todo/edit/:id', to: 'todo#edit'
-    post 'todo/edit/:id', to: 'todo#update'
-    get 'todo/destroy/:id', to: 'todo#destroy'
 end
